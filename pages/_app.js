72a3f52b80a5c8bf415a294a0073074bcddd65d1
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-10-12 19:43:23 -0300

    12/10

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../public/style/globalstyle.css";
 import "../public/style/navBar.css";
 import "../public/style/header.css"
 import "../public/style/card.css"
-
+import "../public/style/creat.css"
 
 export default function MyApp({Component, pageProps}){
  return <Component {...pageProps}/>
---
+++ pages/creat.js
@@ -0,0 +1,13 @@
+import Editor from "../public/components/Editor"
+
+export default function Creat(){
+    return(
+        <div id="creat">
+            <div id="creatContainer">
+                <Editor></Editor>
+                <Editor></Editor>
+            </div>
+        </div>
+    )
+
+}
