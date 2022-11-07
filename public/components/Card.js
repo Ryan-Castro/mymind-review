5652bcb31e3bc768cf6b6a2fb0bd8502483d0124
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-11-07 10:29:42 -0300

    Create Card.js

---
+++ public/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <h1>{props.h1}</h1>
+            <p dangerouslySetInnerHTML={{__html: props.resumo}}></p>
+            <a href={`./reading?id=`+props.h1+"&type="+props.numType}>Ler</a>
+        </div>
+
+    )
+}
