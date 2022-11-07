2f85f2c9e0663d0ad8629489d321b177d6dd22ae
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-11-07 10:02:47 -0300

    Update Card.js

--- public/components/Card.js
+++ public/components/Card.js
@@ -5,7 +5,6 @@ export default function Card(props){
         <div className="card">
             <h1>{props.h1}</h1>
             <p dangerouslySetInnerHTML={{__html: props.resumo}}></p>
-            <a href={`./reading?id=`+props.h1+"&type="+props.numType}>Ler</a>
         </div>
 
     )
